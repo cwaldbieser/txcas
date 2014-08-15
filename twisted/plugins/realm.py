7ac882047dfef471ae596f254950fe3700ccef94
Carl Waldbieser <cwaldbieser@gmail.com>
2014-08-14 21:31:00 -0400

    Added `BasicRealm`, a simple user realm with no attributes.

--- twisted/plugins/realm.py
+++ twisted/plugins/realm.py
@@ -3,6 +3,9 @@
 from txcas.demo_realm import DemoRealmFactory
 demo_realm_factory = DemoRealmFactory()
 
+from txcas.basic_realm import BasicRealmFactory
+basic_realm_factory = BasicRealmFactory()
+
 from txcas.ldap_realm import LDAPRealmFactory
 ldap_realm_factory = LDAPRealmFactory()
 
---
+++ txcas/basic_realm.py
@@ -0,0 +1,65 @@
+
+# Standard library
+from textwrap import dedent
+
+# Application module
+from txcas.interface import ICASUser, IRealmFactory
+
+# External module
+from twisted.cred.portal import IRealm
+from twisted.internet import defer
+from twisted.plugin import IPlugin
+from zope.interface import implements
+
+class User(object):
+
+    implements(ICASUser)
+
+    username = None
+    attribs = None
+    
+    def __init__(self, username, attribs):
+        self.username = username
+        self.attribs = attribs
+   
+    def logout(self):
+        pass 
+
+class BasicRealmFactory(object):
+    """
+    A basic realm factory.
+    """
+    implements(IPlugin, IRealmFactory)
+
+    tag = "basic_realm"
+
+    opt_help = dedent('''\
+            A basic realm that creates an avatar from an ID with no
+            attributes.
+            ''')
+
+    opt_usage = '''This type of realm has no options.'''
+
+    def generateRealm(self, argstring=""):
+        """
+        Produce a BaiscRealm instance.
+        """
+        return BasicRealm() 
+
+class BasicRealm(object):
+    """
+    A Basic user realm that maps an avatar ID to an avatar with a matching
+    username and no attributes.
+    """
+
+    implements(IRealm)
+
+    def requestAvatar(self, avatarId, mind, *interfaces):
+        """
+        """
+        def cb():
+            if not ICASUser in interfaces:
+                raise NotImplementedError("This realm only implements ICASUser.")
+            avatar = User(avatarId, None)
+            return (ICASUser, avatar, avatar.logout)
+        return defer.maybeDeferred(cb)
