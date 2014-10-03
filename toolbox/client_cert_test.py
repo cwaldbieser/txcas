95f80693d33855ba7e3c258734f83c3a04fab8bf
Carl Waldbieser <cwaldbieser@gmail.com>
2014-10-02 20:39:18 -0400

    client test tool-- allow specifying the SSL method; client cert is optional.
    Update the client test tool so it is compatible with the various server options.

--- toolbox/client_cert_test.py
+++ toolbox/client_cert_test.py
@@ -5,22 +5,31 @@ import argparse
 import sys
 
 # External modules
+from OpenSSL import SSL
 from twisted.internet import ssl, task, defer 
 from twisted.python.modules import getModule
 from twisted.web.client import getPage
 
 @defer.inlineCallbacks
 def main(reactor, args):
-    print args
+    kwds = {}
     caCertData = args.ca_cert.read()
-    clientData = args.client_cert.read()
-    clientCertificate = ssl.PrivateCertificate.loadPEM(clientData)
+    client_cert = args.client_cert
+    if client_cert is not None:
+        clientData = args.client_cert.read()
+        clientCertificate = ssl.PrivateCertificate.loadPEM(clientData)
+        kwds['clientCertificate'] = clientCertificate 
     authority = ssl.Certificate.loadPEM(caCertData)
 
     host = args.host
     port = args.port
     netloc = "%s:%d" % (host, port)
-    options = ssl.optionsForClientTLS(unicode(host), authority, clientCertificate)
+    extra_cert_opts = {}
+    if args.ssl_method:
+        m = getattr(SSL, '%s_METHOD' % args.ssl_method)
+        extra_cert_opts['method'] = m
+        kwds['extraCertificateOptions'] = extra_cert_opts 
+    options = ssl.optionsForClientTLS( unicode(host), authority, **kwds)
     s = yield getPage("https://%s/login" % netloc, contextFactory=options)
     print s
 
@@ -33,7 +42,7 @@ if __name__ == '__main__':
         type=argparse.FileType('rb'),
         help='Use PEM formatted CA_CERT when checking *server* certificate signature.')
     parser.add_argument(
-        'client_cert',
+        '--client-cert',
         action='store',
         type=argparse.FileType('rb'),
         help='Use PEM formatted client certificate+private key.  Cert will be presented to server during SSL handshake.')
@@ -53,6 +62,11 @@ if __name__ == '__main__':
         action='store',
         default='/login',
         help='Request RESOURCE.')
+    parser.add_argument(
+        '--ssl-method',
+        action='store',
+        choices=['SSLv23', 'SSLv3', 'TLSv1', 'TLSv1_1', 'TLSv1_2'],
+        help='SSL method.')
 
     args = parser.parse_args()
 
