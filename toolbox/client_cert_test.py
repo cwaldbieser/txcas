116c5246875ff6a998ac22df670885b354a06922
Carl Waldbieser <waldbiec@lafayette.edu>
2014-10-15 12:01:33 -0400

    Updated client test tool to allow default behavior (TLSv1, TLSv1.1, TLSv1.2) or a specific SSL protocol.

--- toolbox/client_cert_test.py
+++ toolbox/client_cert_test.py
@@ -7,22 +7,20 @@ import sys
 # External modules
 from OpenSSL import SSL
 from twisted.internet import ssl, task, defer 
-from twisted.internet.interfaces import IOpenSSLClientConnectionCreator
-from twisted.python.components import proxyForInterface
-from twisted.python.modules import getModule
+#from twisted.internet.interfaces import IOpenSSLClientConnectionCreator
+#from twisted.python.components import proxyForInterface
 from twisted.web.client import getPage
 
-class CustomClientConnectionCreator(proxyForInterface(IOpenSSLClientConnectionCreator)):
-    def __init__(self, ssl_opts, original):
-        self._ssl_opts = ssl_opts
-        super(CustomClientConnectionCreator, self).__init__(original)
-
-    def clientConnectionForTLS(self, tlsProtocol):
-        connection = (super(CustomClientConnectionCreator, self).clientConnectionForTLS(tlsProtocol))
-        ssl_ctx = connection.get_context()
-        ssl_ctx.set_options(self._ssl_opts)
-        return connection
-
+#class CustomClientConnectionCreator(proxyForInterface(IOpenSSLClientConnectionCreator)):
+#    def __init__(self, ssl_opts, original):
+#        self._ssl_opts = ssl_opts
+#        super(CustomClientConnectionCreator, self).__init__(original)
+#
+#    def clientConnectionForTLS(self, tlsProtocol):
+#        connection = (super(CustomClientConnectionCreator, self).clientConnectionForTLS(tlsProtocol))
+#        ssl_ctx = connection.get_context()
+#        ssl_ctx.set_options(self._ssl_opts)
+#        return connection
 
 @defer.inlineCallbacks
 def main(reactor, args):
@@ -38,19 +36,20 @@ def main(reactor, args):
     host = args.host
     port = args.port
     netloc = "%s:%d" % (host, port)
-    ssl_opts = SSL.OP_NO_SSLv2
-    if not args.sslv3:
-        ssl_opts |= SSL.OP_NO_SSLv3
-    if args.no_tlsv1:
-        ssl_opts |= SSL.OP_NO_TLSv1
-    if args.no_tlsv1_1:
-        ssl_opts |= SSL.OP_NO_TLSv1_1
-    if args.no_tlsv1_2:
-        ssl_opts |= SSL.OP_NO_TLSv1_2
-    extraCertificateOptions = {'method': SSL.SSLv23_METHOD}
-    kwds['extraCertificateOptions'] = extraCertificateOptions
+    
+    if args.ssl_method is not None:
+        if args.ssl_method == 'ssl3':
+            extraCertificateOptions = {'method': SSL.SSLv3_METHOD}
+        elif args.ssl_method == 'tls1':
+            extraCertificateOptions = {'method': SSL.TLSv1_METHOD}
+        elif args.ssl_method == 'tls1_1':
+            extraCertificateOptions = {'method': SSL.TLSv1_1_METHOD}
+        elif args.ssl_method == 'tls1_2':
+            extraCertificateOptions = {'method': SSL.TLSv1_2_METHOD}
+        kwds['extraCertificateOptions'] = extraCertificateOptions
+    
     options = ssl.optionsForClientTLS( unicode(host), authority, **kwds)
-    options = CustomClientConnectionCreator(ssl_opts, options) 
+    #options = CustomClientConnectionCreator(ssl_opts, options) 
     s = yield getPage("https://%s/login" % netloc, contextFactory=options)
     print s
 
@@ -84,21 +83,10 @@ if __name__ == '__main__':
         default='/login',
         help='Request RESOURCE.')
     parser.add_argument(
-        '--sslv3',
-        action='store_true',
-        help='Allow SSLv3 (not recommended).')
-    parser.add_argument(
-        '--no-tlsv1',
-        action='store_true',
-        help='Disallow TLSv1.')
-    parser.add_argument(
-        '--no-tlsv1_1',
-        action='store_true',
-        help='Disallow TLSv1.1.')
-    parser.add_argument(
-        '--no-tlsv1_2',
-        action='store_true',
-        help='Disallow TLSv1.2.')
+        '--ssl-method',
+        action='store',
+        choices=['ssl3', 'tls1', 'tls1_1', 'tls1_2'],
+        help='Use *only* the TLS protocol version specified when negotiating the secure connection.')
 
     args = parser.parse_args()
 
